e19f7dd3f03580ffbf39d06a59dd0b5bdf0b6470
webfaisalbd <web.faisal.bd@gmail.com>
2022-06-06 15:22:40 +0600

    local storage

---
+++ storage.js
@@ -0,0 +1,16 @@
+function addToLocalStorage() {
+    const idInput = document.getElementById('storage-id');
+    const id = idInput.value;
+
+
+    const valueInput = document.getElementById('storage-value');
+    const value = valueInput.value;
+
+    // local storage setItem
+    if (id && value) {
+        localStorage.setItem(id, value);
+    }
+    idInput.value = '';
+    valueInput.value = '';
+
+}
